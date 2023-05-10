a6234a6118e7de17416c6287f71e106ee1b5e4fc
henry <63601449+HenryBass@users.noreply.github.com>
2023-05-10 10:57:30 -0400

    .

--- population.py
+++ population.py
@@ -1,16 +1,35 @@
 import matplotlib.pyplot as plt
 
-population = 2
+population = 4432266
+unlimited_population = 4432266
 
-simulation_years = 100
-max_productivity = 0.5
-innovation_per_person = 0.01
+tool_human_equivilance = 101_000
 
+simulation_years = 10000
+max_growth = 0.016
+max_usable_population = 10_000_000
+innovation_per_person = 0.00000000002
+tool_self_improvement = 0.0000000005
+
+unlimited_population_over_time = []
 population_over_time = []
+tool_human_equivilance_over_time = []
+net_intelligence_over_time = []
 
 for i in range(1, simulation_years):
-    population = population * (1 + min(max_productivity, innovation_per_person * population))
-    population_over_time.append(population)
+    tool_human_equivilance *= (1 + (tool_self_improvement * tool_human_equivilance) + (innovation_per_person * population))#(1 + min(max_growth, innovation_per_person * population))
+    population *= (1 + (innovation_per_person * (min(population, max_usable_population))))#(1 + min(max_growth, innovation_per_person * population))
+    unlimited_population *= (1 + innovation_per_person * population)
 
-plt.plot(population_over_time)
+    print(tool_human_equivilance)
+
+    population_over_time.append(min(population, 1e8))
+    unlimited_population_over_time.append(min(unlimited_population, 1e8))
+    tool_human_equivilance_over_time.append(min(tool_human_equivilance, 1e8))
+    net_intelligence_over_time.append(min(tool_human_equivilance + population, 1e8))
+
+plt.plot(population_over_time, color='red')
+plt.plot(unlimited_population_over_time, color='blue')
+plt.plot(tool_human_equivilance_over_time, color='green')
+plt.plot(net_intelligence_over_time, color='purple')
 plt.show()
